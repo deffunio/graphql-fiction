698ffdecd89cfd58453ec7400889cc09b16b435f
artem <artem@deffun.io>
2023-08-13 10:43:00 +0700

    Simple example

--- jvm/src/main/java/io/deffun/jacqueline/Main.java
+++ jvm/src/main/java/io/deffun/jacqueline/Main.java
@@ -1,7 +1,32 @@
 package io.deffun.jacqueline;
 
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.StaticDataFetcher;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String schema = """
+                type Query {
+                    what: String
+                }
+                """;
+        SchemaParser schemaParser = new SchemaParser();
+        TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+        RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring()
+                .type("Query", builder -> builder
+                        .dataFetcher("what", new StaticDataFetcher("Say what again!")))
+                .build();
+        SchemaGenerator schemaGenerator = new SchemaGenerator();
+        GraphQLSchema graphQLSchema = schemaGenerator
+                .makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+        GraphQL graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+        ExecutionResult executionResult = graphQL.execute("{ what }");
+        System.out.println(executionResult.toSpecification());
     }
 }
